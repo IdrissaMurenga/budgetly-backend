53016296bcd4bb1f23d361cca5aa8cc224f5cc79
idrissa murenga <1mulengaidriss@gmail.com>
2025-04-11 10:33:51 +0200

    removed user id in category mongodb schema

--- db/models/categoryModel.js
+++ db/models/categoryModel.js
@@ -2,11 +2,6 @@ import mongoose from "mongoose";
 const {Schema, model} = mongoose
 
 const categorySchema = new Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
     name: {
         type: String,
         required: true
